dc311044626dd16f3b1466686ca83084c512748e
IhorHodun <godunvideo@gmail.com>
2022-08-02 21:56:31 +0300

    01:16:06 React JS 9 Hours

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import {React, useState} from 'react';
+import { FaTrashAlt } from 'react-icons/fa'
 
 const Content = () => {
-  const handleNameChange = () => {
-    const names = ['Bob', 'Kevin', 'Bill', 'Mike', 'Vasia']
-    const int = Math.floor(Math.random() * 5);
-    return names[int];
-  }
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "One half pound of Cocoa Covered Almonds Unsalted"
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2"
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3"
+    },
+  ]);
+
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>
+      <ul>
+        {items.map( (item) =>(
+        <li className="item" key={item.id}>
+          <input
+            type="checkbox"
+            checked={item.checked}
+          />
+          <label>{item.item}</label>
+          <FaTrashAlt 
+            role="button" 
+            tabIndex='0' 
+          />
+        </li>
+        ))}
+      </ul>
     </main>
   );
 };
