be2529308e6564ff9100fa7b8b69ae93e43e1eec
IhorHodun <godunvideo@gmail.com>
2022-08-04 12:23:11 +0300

    https://youtu.be/RVFAyFWO4go?t=6568

--- src/Content.js
+++ src/Content.js
@@ -1,42 +1,35 @@
-import {React, useState} from 'react';
+import {React} from 'react';
 import { FaTrashAlt } from 'react-icons/fa'
 
-const Content = () => {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "One half pound of Cocoa Covered Almonds Unsalted"
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2"
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3"
-    },
-  ]);
-
+const Content = ({items, handleCheck, handleDelete}) => {
   return (
     <main>
-      <ul>
-        {items.map( (item) =>(
-        <li className="item" key={item.id}>
-          <input
-            type="checkbox"
-            checked={item.checked}
-          />
-          <label>{item.item}</label>
-          <FaTrashAlt 
-            role="button" 
-            tabIndex='0' 
-          />
-        </li>
-        ))}
-      </ul>
+      {items.length ? (
+        <ul>
+          {items.map( (item) =>(
+          <li className="item" key={item.id}>
+            <input
+              type="checkbox"
+              onChange={() => handleCheck(item.id)}
+              checked={item.checked}
+            />
+            <label
+              style={(item.checked) ? {textDecoration: 'line-through'} : null }
+              onDoubleClick={() => handleCheck(item.id)}
+            >
+              {item.item}
+            </label>
+            <FaTrashAlt
+              onClick={ () => handleDelete(item.id) }
+              role="button" 
+              tabIndex='0' 
+            />
+          </li>
+          ))}
+        </ul>
+      ) : ( 
+          <p style={{ marginTop: '2rem'}}> Your list is empty.</p>
+      )}
     </main>
   );
 };
